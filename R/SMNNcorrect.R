4b154941e909e7894e0dbc9b113e3851379c31e6
yycunc <39810297+yycunc@users.noreply.github.com>
2019-05-24 14:47:57 -0400

    Version 0.99.0

---
+++ R/SMNNcorrect.R
@@ -0,0 +1,307 @@
+# !/usr/bin/env Rscript
+# 04/26/2019
+#
+#' @title SMNN
+#' 
+#' @description This function SMNNcorrect is designed to perform supervised batch effect correction for scRNA-seq data by first identifying nearest neighbors (NNs) within corresponding clusters (or cell types) and then leveraging information from these NNs.
+#' It takes as input raw expression matrices from two or more batches and a list of the unified cluster labels (output from unifiedClusterLabelling).
+#' It outputs batch-corrected expression matrix for each batch.
+#' @usage SMNNcorrect(..., batch1.cluster.labels, batch2.cluster.labels, num.defined.clusters=1, correct.others=FALSE, k=20, sigma=1, cos.norm.in=TRUE, cos.norm.out=TRUE, var.adj=TRUE, subset.row=NULL, order=NULL, n.jobs=NULL)
+#' @param ... Two or more expression matrices each corresponding to one batch, where each row corresponds to a gene, and each colname correspond to a cell. 
+#' The number and order of rows should be identical across all maxtices (i.e., all batches should have the exact same gene set and in the same order).
+#' @param batch1.cluster.labels is a vector specifying the cluster labels of each cell from batch1. Cells not belonging to any clusters should be set to 0.
+#' @param batch2.cluster.labels is a vector specifying the cluster labels of each cell from batch2. Similarly, cells not belonging to any clusters should be set to 0.
+#' @param num.defined.clusters specifies the number of clusters matched between two or more batches. Default is 1.
+#' @param correct.others is a Boolean variable that defines whether to search nearest neighbors among the cells not belonging to any clusters. Default is FALSE, that is, cells not belonging to any clusters will not be considered as candidate nearest neighbors. 
+#' @param k defines the maximum number of nearest neighbors to be identified. Default is 20.
+#' @param sigma defines the bandwidth of the Gaussian smoothing kernel used to compute the correction vector for each cell. Default is 1.
+#' @param cos.norm.in is a boolean variable that defines whether to do cosine normalization on input data before computing distances between cells.
+#' Default is "TRUE".
+#' @param cos.norm.out is a boolean variable that defines whether to do cosine normalization on output data before computing corrected expression results.
+#' Default is "TRUE".
+#' @param var.adj is a Boolean variable that indicates whether to do variance adjustment on the correction vectors. Default is "TRUE".
+#' @param subset.row NULL, 
+#' @param order is an vector defining the reference batch and the order of the other batches to be corrected.
+#' @param n.jobs specifies the number of parallel jobs. It would be set to the number of cores when \code{n.jobs = NULL}.
+#' @return SMNNcorrect returns the following:
+#'     \item corrected expression matrix for each batch
+#'     \item information regarding NNs between the current batch under correction and the reference batch
+#' @author Yuchen Yang <yyuchen@email.unc.edu>, Gang Li <franklee@live.unc.edu>, Huijun Qian <hjqian@live.unc.edu>, Yun Li <yunli@med.unc.edu>
+#' @examples 
+#' # Load the example data data_SMNN
+#' data("data_SMNN")
+#' 
+#' # Provide the marker genes for cluster matching
+#' markers <- c("Col1a1", "Pdgfra", "Ptprc", "Pecam1")
+#' 
+#' # Specify the cluster labels for each marker gene
+#' cluster.info <- c(1, 1, 2, 3)
+#' 
+#' # Call function unifiedClusterLabelling to identify the corresponding clusters between two batches
+#' matched_clusters <- unifiedClusterLabelling(data_SMNN$batch1.mat, data_SMNN$batch2.mat, features.use = markers, cluster.labels = cluster.info, min.perc = 0.3)
+#' 
+#' # Set python version to be compatible with SMNNcorrect implementation
+#' library(reticulate)
+#' use_python("/nas/longleaf/apps/python/3.5.1/bin/python3")
+#'
+#' # Perform batch effect correction using SMNNcorrect
+#' corrected.results <- SMNNcorrect(data_SMNN$batch1.mat, data_SMNN$batch2.mat, batch1.cluster.labels = matched_clusters[[1]], batch2.cluster.labels = matched_clusters[[2]], num.defined.clusters = 3, k=20, sigma=1, cos.norm.in=TRUE, cos.norm.out=TRUE)
+#' @import reticulate
+#' @importFrom S4Vectors DataFrame Rle
+#' @export
+SMNNcorrect <- function(..., batch1.cluster.labels, batch2.cluster.labels, num.defined.clusters=1, correct.others=FALSE, k=20, sigma=1, cos.norm.in=TRUE, cos.norm.out=TRUE, var.adj=TRUE, subset.row=NULL, order=NULL, n.jobs=NULL){
+    batches <- list(...) 
+    nbatches <- length(batches) 
+    if (nbatches < 2L) { 
+        stop("at least two batches must be specified") 
+    }
+
+    batches.t <- list()
+    for (i in 1:nbatches){
+        batches.t[[i]] <- t(batches[[i]])
+    }
+
+    if (is.null(n.jobs)){
+       multiprocessing = import("multiprocessing")
+       n.jobs <- multiprocessing$cpu_count()
+    }
+    mnnpy <- import("mnnpy")
+    print("Data preparation ...")
+    prep.out <- mnnpy$utils$transform_input_data(datas=batches.t, cos_norm_in=cos.norm.in, cos_norm_out=cos.norm.out, var_index=as.character(c(0:ncol(batches.t[[1]]))), var_subset=subset.row, n_jobs=n.jobs)
+    in.batches <- prep.out[[1]]
+    out.batches <- prep.out[[2]]
+    subset.row <- prep.out[[3]]
+    same.set <- prep.out[[4]]
+
+    # Setting up the order.
+    if (is.null(order)) {
+        order <- seq_len(nbatches)
+    } else {
+        order <- as.integer(order)
+        if (!identical(seq_len(nbatches), sort(order))) { 
+            stop(sprintf("'order' should contain values in 1:%i", nbatches))
+        }
+    }
+   
+    # Setting up the variables.
+    ref <- order[1]
+    ref.batch.in <- in.batches[[ref]]
+    if (!same.set) { 
+        ref.batch.out <- out.batches[[ref]]
+    }
+    
+    ### output for supervised batch correction
+    output <- vector("list", nbatches)
+    output[[ref]] <- t(out.batches[[ref]])
+    mnn.list <- vector("list", nbatches)
+    mnn.list[[ref]] <- DataFrame(current.cell=integer(0), other.cell=integer(0), other.batch=integer(0))
+    original.batch <- rep(ref, nrow(ref.batch.in)) 
+
+    # Looping through the batches.
+    for (b in 2:nbatches) { 
+        target <- order[b]
+        other.batch.in <- in.batches[[target]]
+        if (!same.set) { 
+            other.batch.out <- out.batches[[target]]
+        }
+        
+        # Supervised finding pairs of mutual nearest neighbours.
+        s1_supervised <- integer()
+        s2_supervised <- integer()
+        
+        for(c in 1:num.defined.clusters){
+          sets1 <- NULL          
+          ref.batch.rank <- which(batch1.cluster.labels == c)
+          other.batch.rank <- which(batch2.cluster.labels == c)
+
+          ### MNNs for each subset. The index is python format (starting from 0 ..)
+          print(paste0("Finding the mutual nearest neighbors for cell type ", c, " ..."))
+          sets1 <- mnnpy$utils$find_mutual_nn(data1=ref.batch.in[which(batch1.cluster.labels == c),], data2=other.batch.in[which(batch2.cluster.labels == c),], k1=k, k2=k, n_jobs=n.jobs)
+          
+          s1.trace_back <- integer()
+          for(i in 1:length(unlist(sets1[[1]]))){
+            s1.trace_back <- c(s1.trace_back, ref.batch.rank[unlist(sets1[[1]])[i]+1])
+          }
+          s1_supervised <- c(s1_supervised, s1.trace_back)
+          
+          s2.trace_back <- integer()
+          for(j in 1:length(unlist(sets1[[2]]))){
+            s2.trace_back <- c(s2.trace_back, other.batch.rank[unlist(sets1[[2]])[j]+1])
+          }
+          s2_supervised <- c(s2_supervised, s2.trace_back)
+        }
+        
+        if(correct.others == TRUE){
+          sets1 <- NULL
+          
+          ref.batch.rank <- which(batch1.cluster.labels == 0)
+          other.batch.rank <- which(batch2.cluster.labels == 0)
+          sets1 <- mnnpy$utils$find_mutual_nn(data1=ref.batch.in[which(batch1.cluster.labels == 0),], data2=other.batch.in[which(batch2.cluster.labels == 0),], k1=k, k2=k, n_jobs=n.jobs)
+  
+          s1.trace_back <- integer()
+          for(i in 1:length(unlist(sets1[[1]]))){
+            s1.trace_back <- c(s1.trace_back, ref.batch.rank[unlist(sets1[[1]])[i] + 1])
+          }
+          s1_supervised <- c(s1_supervised, s1.trace_back)
+          
+          s2.trace_back <- integer()
+          for(j in 1:length(sets1[[2]])){
+            s2.trace_back <- c(s2.trace_back, other.batch.rank[unlist(sets1[[2]])[j] + 1])
+          }
+          s2_supervised <- c(s2_supervised, s2.trace_back)
+        }
+        
+        # Change index from R format (starting from 1 ..) to python format (starting from 0 ..)
+        s1 <- as.integer(s1_supervised)
+        s2 <- as.integer(s2_supervised)
+        sets_supervised <- list(first = s1, second = s2)
+
+        # Computing the correction vector
+        print("Computing the correction vector ...")
+        correction.in <- compute_correction(ref.batch.in, other.batch.in, s1, s2, other.batch.in, sigma)
+
+        if (!same.set) {
+            correction.out <- compute_correction(ref.batch.out, other.batch.out, s1, s2, other.batch.in, sigma)
+        }
+       
+        # Adjusting the shift variance; done after any SVD so that variance along the correction vector is purely technical.
+        if (var.adj) { 
+            print("Adjusting the shift variance ...")
+            multiprocessing <- import("multiprocessing")
+            builtins <- import_builtins()
+            np <- import("numpy")
+            p_n <- multiprocessing$Pool(n.jobs)
+            #x= mnnpy$utils$adjust_s_variance(ref.batch.in, other.batch.in, other.batch.in[1,], vect[1,], 0.1)
+            vect <- correction.in
+            scaling <- c()
+            for (i in 1:nrow(vect)){
+                scaling <- c(scaling, adjust_s_variance(ref.batch.in, other.batch.in, other.batch.in[i,], vect[i,], sigma))
+            }
+            scaling.max <- np$fmax(scaling, 1)
+            scaling <- np$ndarray$astype(scaling.max, np$float32)
+            for (i in 1:nrow(correction.in)){
+                correction.in[i,] <- correction.in[i,] * scaling[i]
+            }
+
+            #correction.in <- mnnpy$utils$adjust_shift_variance(ref.batch.in, other.batch.in, correction.in, sigma, n_jobs=1L)
+            if (!same.set) {
+            #x= mnnpy$utils$adjust_s_variance(ref.batch.in, other.batch.in, other.batch.in[1,], vect[1,], 0.1)
+                vect <- correction.out
+                scaling <- c()
+                for (i in 1:nrow(vect)){
+                    scaling <- c(scaling, adjust_s_variance(ref.batch.out, other.batch.out, other.batch.out[i,], vect[i,], sigma))
+                }
+#            scaling = p_n$starmap(mnnpy$utils$adjust_v_worker(ref.batch.in, other.batch.in, 0.1), builtins$zip(other.batch.in, vect), chunksize=floor(nrow(other.batch.in)/n_jobs)+1)
+                scaling.max <- np$fmax(scaling, 1)
+                scaling <- np$ndarray$astype(scaling.max, np$float32)
+                for (i in 1:nrow(correction.in)){
+                    correction.out[i,] <- correction.out[i,] * scaling[i]
+                }
+                #correction.out <- mnnpy$utils$adjust_shift_variance(ref.batch.out, other.batch.out, correction.out, sigma=sigma, n_jobs=1L, subset.row) 
+            }
+        }
+
+        # Applying the correction and expanding the reference batch. 
+        other.batch.in <- other.batch.in + correction.in
+        ref.batch.in <- rbind(ref.batch.in, other.batch.in)
+        if (same.set) {
+            output[[target]] <- t(other.batch.in)
+        } else {
+            other.batch.out <- other.batch.out + correction.out
+            ref.batch.out <- rbind(ref.batch.out, other.batch.out)
+            output[[target]] <- t(other.batch.out)
+        }
+
+        # Storing the identities of the MNN pairs (using RLEs for compression of runs).
+        mnn.list[[target]] <- DataFrame(current.cell=s2, other.cell=Rle(s1), other.batch=Rle(original.batch[s1]))
+        original.batch <- c(original.batch, rep(target, nrow(other.batch.in)))
+    }    
+    
+    # Formatting output to be consistent with input.
+    names(output) <- names(batches)
+    names(mnn.list) <- names(batches)
+    final <- list(corrected=output, pairs=mnn.list)
+    
+    print("Done!")
+    return(final)
+}
+
+compute_correction <- function(data1, data2, mnn1, mnn2, data2_or_raw2, sigma){
+    np <- import("numpy")
+    vect <- data1[mnn1,] - data2[mnn2,]
+    mnn2_py <- as.integer(mnn2-1)
+    mnn_output <- np$unique(mnn2_py, return_counts = TRUE)
+    mnn_index <- mnn_output[[1]]
+    mnn_count <- mnn_output[[2]]
+    shape <- as.integer(c(nrow(data2),ncol(vect)))
+    vect_reduced <- np$zeros(shape, dtype = np$float32)
+    for (i in 1:length(mnn2)){
+        vect_reduced[mnn2[i],] <- vect_reduced[mnn2[i],] + vect[i,]
+    }
+    vect_avg <- np$divide(vect_reduced[mnn_index + 1,], as.matrix(np$ndarray$astype(mnn_count, np$float32), ncol = 1))
+    vect_avg <- as.matrix(vect_avg, ncol = 1)
+
+    dist <- t(apply(data2, 1, FUN = function(x) x%*%t(data2[mnn_index + 1,])))
+    exp_distance <- np$exp(-dist/sigma)
+    density <- np$sum(exp_distance[mnn_index + 1,], axis = 0L)
+    mult <- np$divide(exp_distance, density)
+    total_prob <- np$sum(mult, axis = 1L, keepdims = TRUE)
+    output_mult <- np$dot(mult, vect_avg)
+    correction.in <- np$divide(output_mult, total_prob)
+    
+    return(correction.in)
+}
+
+adjust_s_variance <- function(data1, data2, curcell, curvect, sigma){
+    np <- import("numpy")
+    shape <- as.integer(c(nrow(data1), 2))
+    distance1 <- np$zeros(shape, dtype = np$float32)
+    l2_norm <- np$linalg$norm(curvect)
+    grad <- np$divide(curvect, l2_norm)
+    curproj <- np$dot(grad, curcell)
+    prob2 <- 0
+    totalprob2 <- 0
+    for (samecell in 1:nrow(data2)){
+        sameproj <- np$dot(grad, data2[samecell,])
+        working <- curcell - data2[samecell,]
+        scale <- np$dot(working, grad)
+        working <- working - grad * scale
+        samedist <- np$dot(working, working)
+    #   samedist = mnnpy$utils$sq_dist_to_line(curcell, grad, samecell)
+        sameprob <- np$exp(-samedist / sigma)
+        if (sameproj <= curproj){
+           prob2 <- prob2 + sameprob
+        }
+        totalprob2 <- totalprob2 + sameprob
+    }
+    prob2 <- prob2/totalprob2
+    totalprob1 <- 0
+    for (other in 1:nrow(data1)){
+        othercell <- data1[other,]
+        distance1[other, 1] <- np$dot(grad, othercell)
+        working <- curcell - othercell
+        scale <- np$dot(working, grad)
+        working <- working - grad * scale
+        otherdist <- np$dot(working, working)
+        #otherdist = sq_dist_to_line(curcell, grad, othercell)
+        weight <- np$exp(-otherdist/sigma)
+        distance1[other, 2] <- weight
+        totalprob1 <- totalprob1 + weight
+    }
+    index <- np$argsort(distance1[,1])
+    distance1 <- distance1[index + 1,]
+    target <- prob2 * totalprob1
+    cumulative <- 0
+    ref_quan <- distance1[nrow(distance1), 1]
+    for (i in 1:nrow(distance1)){
+        cumulative <- cumulative + distance1[i,][2]
+        if (cumulative > target){
+           ref_quan <- distance1[i,][1]
+           break
+        }
+    }
+    scaling <- (ref_quan - curproj)/l2_norm
+
+    return(scaling)
+}
+
---
+++ R/unifiedClusterLabelling.R
@@ -0,0 +1,235 @@
+# !/usr/bin/env Rscript
+# 04/26/2019
+#
+#' @title SMNN
+#' 
+#' @description This function unifiedClusterLabelling is used to match the clusters/cell types across multiple scRNA-seq batches. 
+#' It takes as input raw expression matrices from two or more batches, a list of marker genes and their corresponding cluster labels.
+#' It outputs cluster corresponding labels for the cells in each batch.
+#' @usage unifiedClusterLabelling(..., features.use, cluster.labels, datatype="count", ident_list=NULL, cluster.use=NULL, cluster.names=NULL, min.exp.thresh=0, min.perc=0.6, min.average.expr=0)
+#' @param ... Two or more expression matrices where each row corresponds to a gene and each column corresponds to a single cell. All matriecs should contain the same number of rows (i.e., all batches should have the exact same gene set and in the same order).
+#' @param features.use is a vector of marker genes used to match clusters across batches.
+#' @param cluster.labels specifies the corresponding cluster label for each marker gene.
+#' @param datatype defines the type of data, which can be "count", "CPM", "RPKM" and "FPKM". Default is "count".
+#' @param ident_list is a list specifying cluster identities of the cells from each batch.
+#' @param cluster.use defines the clusters used for matching.
+#' @param cluster.names specifies the labels of clusters.
+#' @param min.exp.thresh sets the minimum expression value for each marker genes in each cell. Default is 0.
+#' @param min.perc sets the minimum percentage of cells whose expression level of marker gene \code{>=min.exp.thresh} during cluster definition. Default is 0.6.
+#' @param min.average.expr sets the minimum average expression value of each marker gene during cluster definition. Default is 0.
+#' @return unifiedClusterLabelling returns a list of unified cluster labels for the cells in each batch.
+#' @author Yuchen Yang <yyuchen@email.unc.edu>, Gang Li <franklee@live.unc.edu>, Huijun Qian <hjqian@live.unc.edu>, Yun Li <yunli@med.unc.edu>
+#' @examples 
+#' # Load the example data data_SMNN
+#' data("data_SMNN")
+#' 
+#' # Provide the marker genes for cluster matching
+#' markers <- c("Col1a1", "Pdgfra", "Ptprc", "Pecam1")
+#' 
+#' # Specify the cluster labels for each marker gene
+#' cluster.info <- c(1, 1, 2, 3)
+#' 
+#' # Call function unifiedClusterLabelling to identify the corresponding clusters between two batches
+#' matched_clusters <- unifiedClusterLabelling(data_SMNN$batch1.mat, data_SMNN$batch2.mat, features.use = markers, cluster.labels = cluster.info, min.perc = 0.3)
+#' @import Matrix
+#' @importFrom reshape melt
+#' @export
+unifiedClusterLabelling <- function(..., features.use, cluster.labels, datatype="count", ident_list=NULL, cluster.use=NULL, cluster.names=NULL, min.exp.thresh=0, min.perc=0.6, min.average.expr=0) {
+  group.used_all <- NULL
+  batches <- list(...) 
+  nbatches <- length(batches)
+  if (nbatches < 2L) { 
+    stop("at least two batches must be specified") 
+  }
+  if (is.null(features.use) == 'TRUE'){
+    stop("Error : a list of marker genes is required for cluster matching.")
+  }
+  if (is.null(cluster.labels) == 'TRUE'){
+    stop("Error : a list of cluster labels is required for defining markers for certain clusters.")
+  }
+  if (!is.null(ident_list)){
+    if (length(ident_list) != nbatches) { 
+      stop("Error : ident_list must be of the same length as the number of batches.") 
+    }
+  } else {
+    for (b in 1:nbatches){
+      ident <- seurat_Smnn(batches[[b]], datatype = datatype, SEED = 1)
+      ident_list <- c(ident_list, list(ident))
+    }
+  }
+  
+  for (b in 1:nbatches){
+    data <- batches[[b]]
+    ident <- ident_list[[b]]
+    features.use <- features.use[features.use %in% rownames(data)]
+    if (is.null(cluster.use)){
+      cluster.use_b <- levels(ident)
+    } else {
+      cluster.use_b <- cluster.use
+    }
+    if (is.null(cluster.names)){
+      cluster.names_b <- cluster.use_b
+    } else{
+      cluster.names_b <- cluster.names
+    }
+    if (length(cluster.names_b) != length(cluster.use_b)){
+      print("Error : cluster.names must be of the same length as the clusters.use/ number of clusters. Using cluster numbers as labels ...")
+      cluster.names_b <- cluster.use_b
+    }
+    
+    #Initialize matrix of percent expressing cells
+    PercMat <- matrix(0, nrow = length(features.use), ncol = 0)
+    rownames(PercMat) <- features.use; 
+    
+    #Initialize matrix of average transcript levels
+    ExpMat <- PercMat;
+    
+    #Count mat
+    Count.mat <- data[features.use, colnames(data)]
+    
+    if (length(features.use) > 1){
+      for (i in cluster.use_b){
+        cells.in.cluster <- names(ident)[which(ident == i)]
+        vec.exp <- apply(data[features.use, cells.in.cluster], 1, function(x) sum(x > min.exp.thresh)/length(x)) 
+        PercMat <- cbind(PercMat,vec.exp)
+        
+        vec.exp <- apply(Count.mat[features.use, cells.in.cluster], 1, function(x) if (sum(x > min.exp.thresh) > 1){ mean(x[x > min.exp.thresh]) } else {sum(x)})
+        ExpMat <- cbind(ExpMat, vec.exp)
+      }
+    } else if (length(features.use) == 1){
+      for (i in cluster.use_b){
+        cells.in.cluster <- names(ident)[which(ident == i)]
+        vec.exp <- sum(data[features.use, cells.in.cluster] > min.exp.thresh)/length(data[features.use, cells.in.cluster]) 
+        PercMat <- cbind(PercMat,vec.exp)
+        
+        if (sum(Count.mat[cells.in.cluster] > 0) > 1){
+          vec.exp <- mean(Count.mat[cells.in.cluster][Count.mat[cells.in.cluster] > 0])
+        } else {
+          vec.exp <- sum(Count.mat[cells.in.cluster])
+        }
+        ExpMat <- cbind(ExpMat, vec.exp)
+      }
+    }
+    colnames(ExpMat) <- cluster.names_b
+    colnames(PercMat) <- cluster.names_b
+    
+    #if (!is.null(max.val.perc)) PercMat[PercMat > max.val.perc] = max.val.perc
+    #if (!is.null(max.val.exp)) ExpMat[ExpMat > max.val.exp] = max.val.exp
+    
+    
+    ExpVal <- melt(ExpMat)
+    PercVal <- melt(PercMat)
+    colnames(ExpVal) <- c("gene","cluster","nTrans")
+    ExpVal$percExp <- PercVal$value*100
+    
+    ### define clusters according to corresponding markers of each cell type
+    group.index <- matrix(0, nrow = length(cluster.names_b), ncol = 0)
+    rownames(group.index) <- cluster.names_b
+    for (i in 1:length(cluster.labels)){
+      index <- as.numeric(ExpVal$percExp[which(ExpVal$gene == features.use[i])] >= min.perc*100 & ExpVal$nTrans[which(ExpVal$gene == features.use[i])] >= min.average.expr)
+      group.index <- cbind(group.index, index)
+    }
+    colnames(group.index) <- cluster.labels
+    
+    group.assigned <- rep(0, length(cluster.names_b))
+    for (i in 1:length(cluster.names_b)){
+      if (sum(group.index[i,]) == 0){
+        group.assigned[i] <- 0
+      } else if (sum(group.index[i,]) >= 1){
+        cluster.multi_assigned <- unique(cluster.labels[which(group.index[i,] != 0)])
+        if (length(cluster.multi_assigned) == 1){
+          group.assigned[i] <- cluster.multi_assigned
+        }
+      }
+    }
+    
+    dist.calc <- function(x1, x2){
+      sum <- 0
+      for (i in x1){
+        for (j in x2){
+          sum <- sum + sqrt(sum((i - j) ^ 2))
+        }
+      }
+      dist <- sum/(length(x1) * length(x2))
+      
+      return(dist)
+    } 
+    
+    ### refine cell type labels for those clusters expressing markers of more than one cell types
+    for (i in 1:length(cluster.names_b)){
+      if (sum(group.index[i,]) > 1){
+        cluster.multi_assigned <- cluster.labels[which(group.index[i,] != 0)]
+        if (length(unique(cluster.multi_assigned)) > 1){
+          dist.i <- NULL
+          for (j in 1:length(cluster.multi_assigned)){
+            if (sum(group.assigned == cluster.multi_assigned[j]) == 0){
+              dist <- 0
+            } else {
+              cell.object <- which(ident == i)
+              cell.query <- NULL
+              for (c in cluster.names_b[which(group.assigned == cluster.multi_assigned[j])]){
+                cell.query <- c(cell.query, which(ident == c))
+              }
+              dist <- dist.calc(data[features.use[j],cell.query], data[features.use[j], cell.object])
+            }
+            dist.i <- c(dist.i, dist)
+          }
+          group.assigned[i] <- cluster.multi_assigned[which(dist.i == min(dist.i))]
+        }
+      }
+    }
+    
+    ### assign cell type label to each cell
+    group.used <- rep(0, length(ident))
+    for (c in 1:length(group.assigned)){
+      group.used[which(ident == (c-1))] <- group.assigned[c]
+    }
+    
+    group.used_all <- c(group.used_all, list(group.used))
+  }
+  
+  return(group.used_all)
+}
+
+
+#' @import Seurat
+#' @import dplyr
+#' @import Matrix
+seurat_Smnn <- function(inputTags, datatype = "count", resolution = 0.9, seurat_min_cell=200, resolution_min = 1.2, SEED = 1){
+  seuratOUTPUT <- NULL
+  
+  # Initialize the Seurat object with the raw data (non-normalized data)
+  # Keep all genes expressed in >= 3 cells, keep all cells with >= 200 genes
+  seuratOUTPUT <- CreateSeuratObject(inputTags, min.cells = 0, min.features = 0, project = "single-cell clustering")
+  
+  # Perform log-normalization, first scaling each cell to a total of 1e4 molecules (as in Macosko et al. Cell 2015)
+  if (datatype == "count"){
+    seuratOUTPUT = NormalizeData(object = seuratOUTPUT, normalization.method = "LogNormalize", scale.factor = 10000)
+  } else if (datatype == "CPM" || datatype == "FPKM" || datatype == "RPKM" || datatype == "TPM"){
+    raw.data <- GetAssayData(object = seuratOUTPUT, slot = "raw.data")
+    normalized.data <- log(raw.data+1)
+    colnames(x = normalized.data) <- colnames(x = raw.data)
+    rownames(x = normalized.data) <- rownames(x = raw.data)
+    seuratOUTPUT <- SetAssayData(object = seuratOUTPUT, assay.type = "RNA",slot = "data", new.data = normalized.data)
+  }
+  
+  # Detection of variable genes across the single cells
+  seuratOUTPUT <- FindVariableFeatures(object = seuratOUTPUT, selection.method = "vst")
+  
+  # Regress out unwanted sources of variation
+  seuratOUTPUT <- ScaleData(object = seuratOUTPUT, vars.to.regress = c("nCount_RNA"), verbose = FALSE)
+  
+  ### Perform linear dimensional reduction
+  seuratOUTPUT <- RunPCA(object = seuratOUTPUT, features = VariableFeatures(object = seuratOUTPUT), verbose = FALSE)
+  
+  seuratOUTPUT <- FindNeighbors(object = seuratOUTPUT, dims = 1:20)
+  if (length(inputTags[1,]) >= seurat_min_cell){
+    ### Clustering the cells by Seurat
+    seuratOUTPUT <- FindClusters(object = seuratOUTPUT, algorithm = 3, resolution = resolution, verbose = FALSE, random.seed = SEED)
+  } else {
+    resolution <- resolution_min
+    seuratOUTPUT <- FindClusters(object = seuratOUTPUT, algorithm = 3, resolution = resolution_min, verbose = FALSE, random.seed = SEED)
+  }
+  
+  return(seuratOUTPUT@active.ident)
+}
